3c851863e2788aa39a3f7bf8792d39bab2697be4
teodora <teodorabaciu66@yahoo.com>
2025-10-03 18:08:47 +0100

    commit

--- app/asgi.py
+++ app/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
+settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 application = get_asgi_application()
