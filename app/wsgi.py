647e856df6540538ae06f25e9378b858e37468a1
teodora <teodorabaciu66@yahoo.com>
2025-10-03 20:03:55 +0100

    commit

--- app/wsgi.py
+++ app/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+settings_module = 'app.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'app.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
 application = get_wsgi_application()
